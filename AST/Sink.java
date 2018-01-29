a5090295710ac1d0be2b3b63fe56d9e48f089429
Ritvik <ritvik91@ufl.edu>
2018-01-28 20:58:58 -0500

    Initial Commit

---
+++ AST/Sink.java
@@ -0,0 +1,15 @@
+package cop5556fa17.AST;
+
+import cop5556fa17.Scanner.Token;
+import cop5556fa17.TypeUtils.Type;
+
+public abstract class Sink extends ASTNode {
+	
+	public Type type;
+	
+	public Sink(Token firstToken) {
+		super(firstToken);
+	}
+	
+
+}
---
+++ AST/Statement.java
@@ -0,0 +1,13 @@
+package cop5556fa17.AST;
+
+import cop5556fa17.Scanner.Token;
+import cop5556fa17.TypeUtils.Type;
+
+public abstract class Statement extends ASTNode {
+
+	public Type type;
+	public Statement(Token firstToken) {
+		super(firstToken);
+	}
+
+}
